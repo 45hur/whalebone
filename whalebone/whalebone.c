1728609381d45b16fffe0c7633c1ff8476165f9f
Ashur <Jan.Mikulasek@solarwinds.com>
2019-02-26 20:19:50 +0100

    SOA check 2

--- whalebone/whalebone.c
+++ whalebone/whalebone.c
@@ -106,7 +106,7 @@ int checkDomain(char * qname_Str, int * r, kr_layer_t *ctx, struct ip_addr *user
 
 					debugLog("\"method\":\"getdomain\",\"message\":\"authority for %s\"", querieddomain);
 
-					return explode(ctx, (char *)&querieddomain, &origin, request, last, req_addr);
+					return explode((char *)&querieddomain, userIpAddress, userIpAddressString, rr->type);
 				}
 				else
 				{
