c729fae3dc279138babe66e1d8bc23d1cdbc23e4
Ashur <Jan.Mikulasek@solarwinds.com>
2019-04-18 20:17:46 +0200

    IP Range crc filter, range functionality disabled

--- whalebone/cache_iprange.c
+++ whalebone/cache_iprange.c
@@ -4,8 +4,10 @@
 #include <math.h>      
 #include <unistd.h>
 
+#include "crc64.h"
 #include "iprange.h"
 #include "cache_iprange.h"
+#include "log.h"
 
 cache_iprange* cache_iprange_init(int count)
 {
@@ -18,11 +20,12 @@ cache_iprange* cache_iprange_init(int count)
 	item->capacity = count;
 	item->index = 0;
 	item->searchers = 0;
+	item->base = (unsigned long long *)malloc(item->capacity * sizeof(unsigned long long));
 	item->low = (struct ip_addr **)malloc(item->capacity * sizeof(struct ip_addr *));
 	item->high = (struct ip_addr **)malloc(item->capacity * sizeof(struct ip_addr *));
 	item->identity = (char **)malloc(item->capacity * sizeof(char *));
 	item->policy_id = (int *)malloc(item->capacity * sizeof(int));
-	if (item->low == NULL || item->high == NULL || item->identity == NULL || item->policy_id == NULL)
+	if (item->base == NULL || item->low == NULL || item->high == NULL || item->identity == NULL || item->policy_id == NULL)
 	{
 		return NULL;
 	}
@@ -41,13 +44,25 @@ cache_iprange* cache_iprange_init_ex(struct ip_addr ** low, struct ip_addr ** hi
 	item->capacity = count;
 	item->index = count;
 	item->searchers = 0;
+	item->base = (unsigned long long *)malloc(count * sizeof(unsigned long long));
 	item->low = low;
 	item->high = high;
 	item->identity = identity;
 	item->policy_id = policy_id;
-	if (item->low == NULL || item->high == NULL || item->identity == NULL || item->policy_id == NULL)
+	if (item->base == NULL || item->low == NULL || item->high == NULL || item->identity == NULL || item->policy_id == NULL)
 	{
 		return NULL;
+	}
+
+	for (int i = count - 1; i >= 0; i--)
+	{
+		char ipaddr[20] = {0};
+		unsigned char *addr = (unsigned char *)&(item->low[i]->ipv4_sin_addr);
+
+		sprintf(&ipaddr, "%d.%d.%d.%d", addr[3], addr[2], addr[1], addr[0]);
+		uint64_t crc = crc64(0, (const char *)&ipaddr, strlen(ipaddr));
+		item->base[i] = crc;
+		//debugLog("\"method\":\"cache_iprange_init_ex\",\"ipaddr\":\"%s\",\"crc\":\"%llx\"", ipaddr, crc);
 	}
 
 	return item;
@@ -90,6 +105,11 @@ void cache_iprange_destroy(cache_iprange *cache)
 		//  	}
 	}
 
+	if (cache->base != NULL)
+	{
+		free(cache->base);
+		cache->base = NULL;
+	}
 	if (cache->low != NULL)
 	{
 		free(cache->low);
@@ -149,7 +169,45 @@ int cache_iprange_add(cache_iprange* cache, struct ip_addr *low, struct ip_addr 
 	return 0;
 }
 
-int cache_iprange_contains(cache_iprange* cache, const struct ip_addr * ip, iprange *item)
+int cache_iprange_contains(cache_iprange* cache, const struct ip_addr * ip, const char * ipaddr, iprange *item)
+{
+	if (cache == NULL)
+	{
+		return 0;
+	}
+
+	cache->searchers++;
+	int lowerbound = 0;
+	int upperbound = cache->index;
+	int position;
+	unsigned long long value = crc64(0, ipaddr, strlen(ipaddr));
+
+	position = (lowerbound + upperbound) / 2;
+
+	while ((cache->base[position] != value) && (lowerbound <= upperbound))
+	{
+		if (cache->base[position] > value)
+		{
+			upperbound = position - 1;
+		}
+		else
+		{
+			lowerbound = position + 1;
+		}
+		position = (lowerbound + upperbound) / 2;
+	}
+
+	if (lowerbound <= upperbound)
+	{
+		item->identity = cache->identity[position];
+		item->policy_id = cache->policy_id[position];
+	}
+
+	cache->searchers--;
+	return (lowerbound <= upperbound);
+}
+
+int cache_iprange_contains_old(cache_iprange* cache, const struct ip_addr * ip, iprange *item)
 {
 	if (cache == NULL)
 	{
