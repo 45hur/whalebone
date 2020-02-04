6b11425fbebdd48763191be680cfc5bc6eddb576
Ashur <mikulasek@live.com>
2020-02-04 12:49:28 +0100

    Batch

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 docker stop resolver
 docker rm resolver
 docker build . -t resolver
-docker run -it -v C:\var\whalebone:/var/whalebone -v C:\var\log\whalebone:/var/log/whalebone -e DEBUGLOG=1 --cap-add=NET_RAW --cap-add=NET_ADMIN --net=host --name=resolver resolver
+docker run -it -v C:\var\whalebone:/var/whalebone -v C:\var\log\whalebone:/var/log/whalebone -e DEBUGLOG=1 -e LOG=1 --cap-add=NET_RAW --cap-add=NET_ADMIN --net=host --name=resolver resolver
